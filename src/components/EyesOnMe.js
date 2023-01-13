1c253c39b143d44502c9ee0e46974399ad111d51
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-01-13 07:28:25 +0300

    Done with assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+function EyesOnMe () {
+    function focusBtn () {
+        return console.log("Good!")
+    } 
+
+    function blurBtn () {
+        return console.log("Hey! Eyes on me!")
+    }
+    return (
+            <button onFocus={focusBtn} onBlur={blurBtn}>"Eyes on me"</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,13 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function inputchange() {
+        console.log("Entering password...")
+    } 
     return (
-        <div></div>
-    )
+        <input type="password" onChange={inputchange}/>
+        )
 }
 
 export default Keypad;
